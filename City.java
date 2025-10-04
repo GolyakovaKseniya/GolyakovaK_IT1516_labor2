803712e0eabc499211761f505397184d99f9842f
ReshetnikovaGolyakovaIT1516 <golakovak3@gmail.com>
2025-10-04 14:01:51 +0500

    Initial commit

---
+++ City.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+public class City {
+    private String nameCity;
+    private Map<String, Integer> ways;
+    private InputChecks check = new InputChecks();
+    public String getNameCity() {
+        return nameCity;
+    }
+    public Map<String, Integer> getWays() {
+        return ways;
+    }
+    public Integer getCost(String nameCity){
+        return ways.get(nameCity);
+    }
+    public void setNameCity(String nameCity) {
+        this.nameCity = nameCity;
+    }
+    //конструктор с названием города
+    public City(String nameCity){
+        this.nameCity = nameCity;
+        this.ways = new HashMap<>();
+    }
+    //конструктор с набором путей для 2 задачи
+    public City(String nameCity, Map<String,Integer> way){
+        this.nameCity = nameCity;
+        this.ways = new HashMap<>(way);
+    }
+    //добавление маршрута и стоимости
+    public void addRoute(String nameCity, int cost){
+        cost = check.negativeNumber(cost);
+        ways.put(nameCity, cost);
+    }
+    @Override
+    public String toString() {
+        return nameCity + ":" + ways;
+    }
+}
---
+++ InputChecks.java
@@ -0,0 +1,23 @@
+public class InputChecks {
+    public double point(double number){
+        if(Math.abs(number) >= 1000000){
+            System.out.println(number + " - cлишком большое число! Будет установлено значение 10000");
+            return 10000;
+        }
+        return number;
+    }
+    public int fraction(int denominator){
+        if(denominator == 0){
+            System.out.println("Знаменатель не может быть равен 0. Установлен знаменатель = 1");
+            return 1;
+        }
+        return denominator;
+    }
+    public Integer negativeNumber(int number){
+        if(number < 0){
+            System.out.println("Стоимость не может быть меньше 0. Установлено значение 0");
+            return 0;
+        }
+        return number;
+    }
+}
